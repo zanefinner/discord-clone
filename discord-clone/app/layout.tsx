df0cc526cc275ea33fd7a532eae7b6b53de5fdf5
zane finner <zanefinnergm@gmail.com>
2025-02-12 18:40:23 -0500

    themes

--- discord-clone/app/layout.tsx
+++ discord-clone/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs';
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const font = Open_Sans({
 
@@ -20,11 +21,19 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={`${font.className} ${font.className} antialiased`}
         >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            storageKey="discord-theme"
+          >
           {children}
+          </ThemeProvider>
+          
         </body>
       </html>
     </ClerkProvider>
